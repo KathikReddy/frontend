86ba400230c07878238864d56e2dd0be3563b5dd
KathikReddy <163815669+KathikReddy@users.noreply.github.com>
2025-09-24 19:33:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  // use relative paths
   plugins: [react()],
 })
